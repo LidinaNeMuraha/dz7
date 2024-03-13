508d677da0fa4d60dca3ad776c98632b83bd8deb
Александр <kmuken@bk.ru>
2024-03-14 00:16:27 +0500

    peredelal nazvaniia peremennih3

--- src/Main.java
+++ src/Main.java
@@ -8,66 +8,66 @@ public class Main {
 
         // zadacha 1 2
 
-        int[] zadachaOdin = new int[3];
-        zadachaOdin[0] = 1;
-        zadachaOdin[1] = 2;
-        zadachaOdin[2] = 3;
+        int[] arrayOne = new int[3];
+        arrayOne[0] = 1;
+        arrayOne[1] = 2;
+        arrayOne[2] = 3;
 
-        double[] zadachaOdinDva = {1.57, 7.654, 9.986};
+        double[] arrayTwo = {1.57, 7.654, 9.986};
 
-        int[] zdachaOdinTri = {44, 55, 66};
+        int[] arrayThree = {44, 55, 66};
 
-        System.out.println(Arrays.toString(zadachaOdin));
+        System.out.println(Arrays.toString(arrayOne));
 
-        System.out.print(zadachaOdin[0] + ", ");
-        System.out.print(zadachaOdin[1] + ", ");
-        System.out.print(zadachaOdin[2]);
-
-        System.out.println();
-
-        System.out.print(zadachaOdinDva[0] + ", ");
-        System.out.print(zadachaOdinDva[1] + ", ");
-        System.out.print(zadachaOdinDva[2]);
+        System.out.print(arrayOne[0] + ", ");
+        System.out.print(arrayOne[1] + ", ");
+        System.out.print(arrayOne[2]);
 
         System.out.println();
 
-        System.out.print(zdachaOdinTri[0] + ", ");
-        System.out.print(zdachaOdinTri[1] + ", ");
-        System.out.print(zdachaOdinTri[2]);
+        System.out.print(arrayTwo[0] + ", ");
+        System.out.print(arrayTwo[1] + ", ");
+        System.out.print(arrayTwo[2]);
+
+        System.out.println();
+
+        System.out.print(arrayThree[0] + ", ");
+        System.out.print(arrayThree[1] + ", ");
+        System.out.print(arrayThree[2]);
 
         System.out.println();
         System.out.println();
 
         // zadacha 3
 
-        System.out.print(zadachaOdin[2] + ", ");
-        System.out.print(zadachaOdin[1] + ", ");
-        System.out.print(zadachaOdin[0]);
+        System.out.print(arrayOne[2] + ", ");
+        System.out.print(arrayOne[1] + ", ");
+        System.out.print(arrayOne[0]);
 
         System.out.println();
 
-        System.out.print(zadachaOdinDva[2] + ", ");
-        System.out.print(zadachaOdinDva[1] + ", ");
-        System.out.print(zadachaOdinDva[0]);
+        System.out.print(arrayTwo[2] + ", ");
+        System.out.print(arrayTwo[1] + ", ");
+        System.out.print(arrayTwo[0]);
 
         System.out.println();
 
-        System.out.print(zdachaOdinTri[2] + ", ");
-        System.out.print(zdachaOdinTri[1] + ", ");
-        System.out.print(zdachaOdinTri[0]);
+        System.out.print(arrayThree[2] + ", ");
+        System.out.print(arrayThree[1] + ", ");
+        System.out.print(arrayThree[0]);
 
         // zadacha 4
 
-        for (int i = 0; i < zadachaOdin.length; i++) {
-            if (zadachaOdin[i] % 2 == 0) {
+        for (int i = 0; i < arrayOne.length; i++) {
+            if (arrayOne[i] % 2 == 0) {
 
-            } else if (zadachaOdin[i] % 2 == 1)
-                zadachaOdin[i] = zadachaOdin[i] + 1;
+            } else if (arrayOne[i] % 2 == 1)
+                arrayOne[i] = arrayOne[i] + 1;
 
         }
         System.out.println( );
         System.out.println( );
-        System.out.println(Arrays.toString(zadachaOdin));
+        System.out.println(Arrays.toString(arrayOne));
 
         // chast 2 zadanie 1
 
